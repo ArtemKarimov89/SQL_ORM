bd2abe75856534a128b8cadf9c5bb8e01a845b2b
Artem <karimov.art89@gmail.com>
2024-04-17 21:55:23 +0300

    refactoring after mentor's review

--- main.py
+++ main.py
@@ -1,52 +1,58 @@
 import json
-import datetime
-
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
-
 from Models import create_tables, Publisher, Shop, Book, Stock, Sale
 
-print('Type username:')
-username = input()
-print('Type password:')
-password = int(input())
-print('Type SQL base name:')
-SQL_base_name = input()
 
-DNS = f'postgresql://{username}:{password}@localhost:5432/{SQL_base_name}'
+def create_session():
+    print('Type username:')
+    username = input()
+    print('Type password:')
+    password = int(input())
+    print('Type SQL base name:')
+    SQL_base_name = input()
+    DNS = f'postgresql://{username}:{password}@localhost:5432/{SQL_base_name}'
+    engine = sa.create_engine(dns)
+    create_tables(engine)
+    Session = sessionmaker(bind=engine)
+    new_session = Session()
+    return new_session
 
-engine = sa.create_engine(DNS)
-create_tables(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+def write_data_into_bd(session):
+    with open('tests_data.json', 'r') as f:
+        data = json.load(f)
+    model_dict = {
+        'publisher': Publisher,
+        'shop': Shop,
+        'book': Book,
+        'stock': Stock,
+        'sale': Sale
+    }
+    for data_line in data:
+        model = model_dict[data_line.get('model')]
+        session.add(model(id=data_line.get('pk'), **data_line.get('fields')))
+    session.commit()
 
-with open('tests_data.json', 'r') as f:
-    data = json.load(f)
 
-model_dict = {
-    'publisher': Publisher,
-    'shop': Shop,
-    'book': Book,
-    'stock': Stock,
-    'sale': Sale
-}
+def get_shop(input_data):
+    query = sa.select(Book.title, Shop.name, Sale.price, Sale.date_sale).select_from(Book) \
+        .join(Publisher, Publisher.id == Book.id_publisher) \
+        .join(Stock, Stock.id_book == Book.id) \
+        .join(Shop, Shop.id == Stock.id_shop) \
+        .join(Sale, Sale.id == Stock.id)
+    if input_data.isdigit():
+        query = query.where(Publisher.id == input_data)
+    else:
+        query = query.where(Publisher.name == input_data)
+    data_need = session.execute(query).all()
 
-for data_line in data:
-    model = model_dict[data_line.get('model')]
-    session.add(model(id=data_line.get('pk'), **data_line.get('fields')))
-session.commit()
+    for book_name, shop_name, sale_price, sale_data in data_need:
+        print(f'{book_name:<40} | {shop_name:<5} | {sale_price:<5} | {sale_data.strftime("%Y-%m-%d")}')
 
-print('Write publisher id:')
-publisher_id = int(input())
 
-querry = sa.select(Book.title, Shop.name, Sale.price, Sale.date_sale).select_from(Book)\
-    .join(Publisher, Publisher.id == Book.id_publisher)\
-    .join(Stock, Stock.id_book == Book.id)\
-    .join(Shop, Shop.id == Stock.id_shop)\
-    .join(Sale, Sale.id == Stock.id).where(Publisher.id == publisher_id)
-
-data_need = session.execute(querry).all()
-
-for data_str in data_need:
-    print(f'{data_str[0]:40} | {data_str[1]:5} | {float(data_str[2]):<5} | {data_str[3].strftime("%Y-%m-%d")}')
+if __name__ == '__main__':
+    session = create_session()
+    write_data_into_bd(session)
+    input_data = input('Write publisher id or name: ')
+    get_shop(input_data)
